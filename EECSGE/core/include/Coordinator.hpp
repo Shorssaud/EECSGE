7c49b2543a1d59114ebbbee4eb6d7ed238859b3a
Shorssaud <shaun.orssaud@epitech.eu>
2023-02-14 21:39:27 +0100

    FIX test3

--- EECSGE/core/include/Coordinator.hpp
+++ EECSGE/core/include/Coordinator.hpp
@@ -21,26 +21,22 @@ public:
 
 
 	// Component methods
-	template<typename T>
 	void RegisterComponent();
 
 	template<typename T>
 	void AddComponent(Entity entity, T component);
 
-	template<typename T>
 	void RemoveComponent(Entity entity);
 
 	template<typename T>
 	T& GetComponent(Entity entity);
 
-	template<typename T>
 	ComponentType GetComponentType();
 
 	// System methods
 	template<typename T>
 	std::shared_ptr<T> RegisterSystem();
 
-	template<typename T>
 	void SetSystemSignature(Signature signature);
 private:
 	std::unique_ptr<ComponentManager> mComponentManager;
