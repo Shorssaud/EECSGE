d0bde39c5ed731c708e1415ce04b6e34dd542a56
Shorssaud <shaun.orssaud@epitech.eu>
2023-02-22 21:08:59 +0100

    UPD replaced components and systems

--- EECSGE/components/Components.hpp
+++ EECSGE/components/Components.hpp
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "collision.hpp"
 #include "drawable.hpp"
-#include "health.hpp"
-#include "input.hpp"
-#include "movement.hpp"
 #include "sound.hpp"
-#include "transform.hpp"
+#include "rigidBody.hpp"
--- EECSGE/systems/include/death.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "EECSGE/core/include/System.hpp"
-#include "EECSGE/components/health.hpp"
-
-class DeathSystem : public System {
-    public:
-        void Update();
-};
