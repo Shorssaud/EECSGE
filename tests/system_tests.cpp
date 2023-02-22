158ee81251abbbecf8a6b50477adfd133c5e7c84
Shorssaud <shaun.orssaud@epitech.eu>
2023-02-22 21:39:16 +0100

    ADD delete test

--- tests/system_tests.cpp
+++ tests/system_tests.cpp
@@ -1,5 +1,6 @@
 #include <EECSGE/EECSGE.hpp>
 #include <gtest/gtest.h>
+#include <vector>
 
 Coordinator gCoordinator;
 
@@ -45,4 +46,23 @@ TEST(Entities, MovementTest) {
 		physics->Update();
 	}
 	EXPECT_EQ(gCoordinator.GetComponent<RigidBody>(e1).position.x, 100);
+	gCoordinator.DestroyEntity(e1);
+}
+
+TEST(Entities, RemovalTest) {
+	std::vector<Entity> entities;
+	// add 6 entities to coordinator
+	for (int i = 0; i < 6; i++) {
+		entities.push_back(gCoordinator.CreateEntity());
+	}
+	gCoordinator.DestroyEntity(entities[1]);
+	// remove all entities from coordinator
+	while(entities.size() > 0) {
+		std::cerr << "Destroying entity " << entities[0] << std::endl;
+		gCoordinator.DestroyEntity(entities[0]);
+		// output on test logs to show that entities are being removed
+		std::cerr << "Entity " << entities[0] << " removed" << std::endl;
+		entities.erase(entities.begin());
+	}
+	EXPECT_EQ(entities.size(), 0);
 }
