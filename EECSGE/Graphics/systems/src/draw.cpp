ac77c94d8ecb50782b8d32c83d5b2f5741ffbc87
Shorssaud <shaun.orssaud@epitech.eu>
2023-03-15 17:15:15 +0100

    DEL oversetting

--- EECSGE/Graphics/systems/src/draw.cpp
+++ EECSGE/Graphics/systems/src/draw.cpp
@@ -10,7 +10,6 @@ void DrawSystem::DrawEntities(sf::RenderWindow *window)
 		auto& rigidBody = gCoordinator.GetComponent<RigidBody>(entity);
 		auto& drawable = gCoordinator.GetComponent<Drawable>(entity);
 		drawable.sprite.setPosition(sf::Vector2f{rigidBody.position.x + rigidBody.velocity.x, rigidBody.position.y + rigidBody.velocity.y});
-		drawable.sprite.setTextureRect(drawable.rect);
 		drawable.sprite.setScale(rigidBody.scale);
 
 		window->draw(drawable.sprite);
